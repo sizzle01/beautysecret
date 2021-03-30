e9ee7f04e523f01026930874add5597f3bbf0f96
Your Name <you@example.com>
2021-03-30 21:55:24 +0100

    slider added

---
+++ src/components/Breaker/Breaker.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./breaker.css";
+
+const Breaker = ({}) => {
+  return (
+    <div className="breaker-cont">
+      <Typography variant="h2"></Typography>
+    </div>
+  );
+};
+
+export default Breaker;
