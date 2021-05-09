01ed2edbfa10f00fae21ab170bc3c61e96e7fbee
Your Name <you@example.com>
2021-05-09 07:54:25 +0100

    adding services texts

--- src/Utils/data.js
+++ src/Utils/data.js
@@ -224,8 +224,124 @@ export default {
       tag: "COSMETIC STORE",
       image: "/images/cosmetics/cosmetics.jpg",
 
-      title: "",
+      title: "We provide a range of safe to use cosmetic products ",
       caption: "",
+    },
+    {
+      tag: "MASSAGE",
+      image: "/images/massage/massage3.jpg",
+
+      title: "MASSAGE SERVICES",
+      caption:
+        "Massage is the working of superficial and deeper layers of muscle and connective tissue using various techniques to enhance function, aid in the healing process, and promote relaxation and well-being. We offer Swedish, Deep Tissue, Sports, Aromatherapy, Prenatal, Reflexology, Scalp and Hot Stone massage.",
+
+      subservicehead: "SWEDISH",
+      subservicetext:
+        "Swedish massage is a European method using long, flowing strokes and kneading to stimulate circulation, improve muscular relaxation and relieve tension.",
+      subservicehead2: "DEEP TISSUE",
+      subservicetext2:
+        "Deep tissue massage is designed to relieve severe tension in the muscle and the connective tissue or fascia. This type of massage focuses on the muscles located below the surface of the top muscles.",
+      subservicehead22: "AROMATHERAPY",
+      subservicetext22:
+        "Aromatherapy is the ancient science of healing, relaxing and energizing by the use of plants and their parts. Essential oils form the basis of aromatherapy.",
+      subservicehead222: "PRENATAL",
+      subservicetext222:
+        "Prenatal massage shares many of the goals of regular massage to relax tense muscles, ease sore spots, improve circulation and mobility, and just make you feel good. But it's also tailored specifically to the needs of pregnant women and their changing bodies.",
+
+      subservicehead3: "REFLEXOLOGY",
+      subservicetext3:
+        "Reflexology is an ancient healing art based on the principles that there are reflexes in the hands and feet that correspond to every part, organ and gland in the body.",
+      subservicehead4: "AYURVEDIC HEAD MASSAGE",
+      subservicetext3:
+        "Massaging the scalp can help stimulate the nerves and blood vessels beneath the skin while calming muscle tension around the head.",
+      subservicehead5: "HOT STONE",
+      subservicetext5:
+        "Hot stone massage is a specialty massage where the therapist uses smooth, heated stones, either as an extension of their own hands, or by placing them on the body while they massage other parts of the body. The heat can be both deeply relaxing and help warm up tight muscles so the therapist can work more deeply, more quickly.",
+    },
+    {
+      tag: "FACIAL TREATMENT",
+      image: "/images/face/facial1.jpg",
+
+      title: "Face treatment",
+      caption:
+        "Target your most pressing skin concerns and improve the health of your skin with a professional face treatment. Through your face mapping, zone-by-zone skin analysis, we'll create a treatment that's customized to meet the needs of your skin, and using professional dermatological products, we'll deliver healthier skin with a fresh appearance. We offer Vitamin C Facial, Deep cleanse Facial, Gold Collagen facial, Hydro Glow facial, Age Smart Treatment Facial, Men's Skin Treatment, and Dermamelan Chemical Peels.",
+
+      subservicehead: "VITAMIN C FACIAL",
+      subservicetext:
+        "This custom facial is tailored to suite your skins needs. It starts with a thorough cleansing, extraction and evaluation. This facial includes a delightful neck, face and decollate massage, exfoliation, extractions and tailored mask. Leaves your skin healthy, soothed and hydrated.",
+      subservicehead2: "AGE SMART TREATMENT FACIAL",
+      subservicetext2:
+        "This treatment is designed to help firm, smooth, nourish, regenerate and energize while controlling the biochemical triggers that lead to skin aging! Potent vitamin and retexurizes skin while energizing massage techniques release tension in the facial muscles for the ultimate in skin therapy for prematurely aging, mature or devitalized skin.",
+      subservicehead22: "MEN's SKIN TREATMENT",
+      subservicetext22:
+        "Designed especially for him, this treatment helps combat the effects of daily shaving, while calming redness and the effects from ingrown hairs. Leaves your skin cleansed and hydrated.",
+      subservicehead222: "EXPRESS FACIAL",
+      subservicetext222:
+        "This condensed version of the signature facial can be adapted to all skin types.",
+
+      subservicehead3: "DEEP CLEANS FACIAL",
+      subservicetext3:
+        "Focus on purifying your skin with deep cleansing and extractions. Formulated to improve skin clarity and reduce blemishes while soothing redness and inflammation.",
+      subservicehead4: "WET/DRY MICRODERMABRASION",
+      subservicetext4:
+        "Make your appointment today for the most forward thinking microdermabrasion service, the Bio-Brasion TRINITYTM Wet/Dry Microdermabrasion with manually applied solutions. Featuring a 3 pass target based approach that provides complete comfort and beautiful results even after the first service.",
+      subservicehead5: "WHAT IS CHEMICAL PEEL?",
+      subservicetext5:
+        "Cosmelan Peel is a technique used to improve and smooth the texture of the skin. Chemical peels are intended to remove the outermost layers of the skin. To accomplish this task the chosen peel solution induces a controlled injury to the skin. This is a powerful skin resurfacing system from the skin health experts at our MedSpa.It helps reduce acne, pigmentation and signs of premature aging to reveal smoother, brighter and soft skin.",
+      subservicehead5: "WHY YOU NEED IT?",
+      subservicetext5:
+        "Chemical peels are one of the most popular professional skin care services today. They can improve the quality, texture and tone of the skin. With Cosmelan, Acnelan, Dermamelan, and Mandelic TCA Peel, you can get powerful results without the associated risks or downtime that other peels may produce. In fact, with the mesoesthetic chemical Peels your skin will look and feel healthier than ever!",
+      subservicehead6: "WHAT CAN I EXPECT?",
+      subservicetext6:
+        "Most clients experience noticeable results within 2-3 treatments, from fewer wrinkles and fine lines to smoother texture and reduced breakouts.",
+    },
+    {
+      tag: "VITAMIN SKIN INFUSIONS",
+      image: "/images/infusion/infusion.jpg",
+
+      title: "Skin infusion",
+
+      caption:
+        " BENEFITS: Vitamin injections allow your body to utilize 100% of the nutrients since they are administered directly into your bloodstream, it helps to maintain skin grid texture, give radiant and even-tone skin.We offer Glutathione injections, Flawless Skin, Detox programs, collagen and l-ascorbic injections…",
+    },
+    {
+      tag: "LASER TREATMENTS",
+      image: "/images/medspa/medspa1.png",
+
+      title: "laser treatments",
+      caption: "",
+      subservicehead: "Laser Hair Removal",
+      subservicetext:
+        "This is a procedure that uses scientifically developed frequencies of light that are absorbed by the pigment in the hair which extinguishes the hair follicles. The ND-YAG laser machine is used for this procedure.",
+      subservicehead2: "Laser Spot Removal ",
+      subservicetext2:
+        " involves the use of laser machines producing light designed to absorb pigments causing them to gradually disappear. The Q-SWITCH laser machine is used for this procedure.",
+      subservicehead22: "Laser Scar Removal ",
+      subservicetext22:
+        " it is the use of laser to remove old, damaged layer of the skin so that a new healthier skin can grow in its place. The CO2 laser machine is used for this procedure.",
+      subservicehead3: "Pain Management (Acupuncture & Cupping therapy)",
+      subservicetext3:
+        " This is a soothing treatment that involves a minimally invasive method to stimulate nerves on the skin surface and influence tissues, glands, and organs of the body. It also stimulates the immune system, promoting circulation to the area, modulating pain, anxiety, depression, hypertension, insomnia, migraines, strokes, chronic pains often in the neck, back, knees and head. We also offer acupuncture and cupping massage.",
+    },
+    {
+      tag: "WAXING",
+      image: "/images/",
+
+      title: "laser treatments",
+      caption: "",
+      subservicehead: "Laser Hair Removal",
+      subservicetext:
+        "We offer waxing services which are hair removal from the follicle by using advanced waxing methods. We offer 1/2 Leg, 3/4 Leg, Full Leg, Bikini, Brazilian, Underarm, Half Arm, Full Arm, Chest, Lip, Chin, Abdomen, Back and Eye Brows waxing. This waxing method is smooth and very beneficial to people who have sensitive skin.",
+    },
+    {
+      tag: "MOROCCAN HAMMAM",
+      image: "/images/hammam/hammam.jpg",
+
+      title: "Turkish bath",
+      caption:
+        "A hammam, also known as a Turkish bath, is a form of steam bath or a public bathing facility associated with Islam. It is a popular characteristic of Muslim culture, and it was derived from the Roman thermae model combined with Central Asian steam bath culture.",
+      subservicehead: "Laser Hair Removal",
+      subservicetext: "",
     },
   ],
 };
--- src/components/Pages/Portfolio/Portfolio.js
+++ src/components/Pages/Portfolio/Portfolio.js
@@ -16,165 +16,200 @@ import {
 } from "@material-ui/core";
 import { useState } from "react";
 import data from "../../../Utils/data";
-
 import "./portfolio.css";
-
+import Banner1 from "../../../asset/images/aboutbanner.jpg";
+import Footer from "../../Footer/Footer";
 const Portfolio = () => {
   const [tabValue, setTabValue] = React.useState();
-  const [projectDialog, setProjectDialog] = useState(false);
 
   return (
-    <Grid container spacing={1} className="section pb_45 pt_45">
-      {/**tabs */}
-      <Grid Item xs={12}>
-        <Tabs
-          value={tabValue}
-          indicatorColor="white"
-          className="customTabs"
-          onChange={(event, newValue) => setTabValue(newValue)}
-        >
-          <Tab
-            /*label="Services"*/
-            className={
-              tabValue == "All" ? "customTabs_item active" : "customTabs_item"
-            }
-          />
-          {[...new Set(data.services.map((item) => item.tag))].map((tag) => (
+    <>
+      <div className="banner" style={{ backgroundImage: `url(${Banner1})` }}>
+        <Typography variant="h2" className="head">
+          Our services
+        </Typography>
+      </div>
+      <div className="service-intro">
+        <Typography variant="h5">
+          Book Our Services and discover ultimate luxurious relaxation,
+          rejuvenating parley, and reinvigorating experience from the moment you
+          walk through the gates, you'll be treated like royalty, with every
+          detail designed to provide the ultimate in comfort and luxury during
+          your stay.
+        </Typography>
+      </div>
+
+      <Grid container spacing={1} className="section pb_45 pt_45">
+        {/**tabs */}
+        <Grid Item xs={12}>
+          <Tabs
+            value={tabValue}
+            indicatorColor="white"
+            className="customTab"
+            onChange={(event, newValue) => setTabValue(newValue)}
+          >
             <Tab
-              label={tag}
-              value={tag}
+              /*label="All"*/
               className={
-                tabValue == tag ? "customTabs_item active" : "customTabs_item"
+                tabValue == "All" ? "customTab_item active" : "customTab_item"
               }
             />
-          ))}
-        </Tabs>
-      </Grid>
-      {/**projects */}
-      <Grid Item xs={12}>
-        <Grid container spacing={3}>
-          {data.services.map((services) => (
-            <>
-              {tabValue == services.tag || tabValue == "All" ? (
-                <Grid Item className="projbox" className="service-wrapper">
-                  <Card className="customCard" xs={12}>
-                    <CardActionArea>
-                      <CardMedia
-                        className="customCard_image"
-                        image={services.image}
-                        title={services.title}
-                      />
-                      <CardContent>
-                        <Typography variant="h5" className="customCard_title">
-                          {services.title}
-                        </Typography>
-
-                        <Typography
-                          variant="body2"
-                          className="customCard_caption"
-                        >
-                          {services.caption}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          className="customCard_caption"
-                        >
-                          {services.captionlist}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          className="customCard_caption"
-                        >
-                          {services.captionlist1}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          className="customCard_caption"
-                        >
-                          {services.captionlist2}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          className="customCard_caption"
-                        >
-                          {services.captionlist3}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          className="customCard_caption"
-                        >
-                          {services.captionlist4}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          className="customCard_caption"
-                        >
-                          {services.captionlist5}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          className="customCard_caption"
-                        >
-                          {services.captionlist6}
-                        </Typography>
-                        <Typography variant="h5" className="subservice-head">
-                          {services.subservicehead}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          className="customCard_caption"
-                        >
-                          {services.subservicetext}
-                        </Typography>
-                        <Typography variant="h5" className="subservice-head">
-                          {services.subservicehead2}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          className="customCard_caption"
-                        >
-                          {services.subservicetext2}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          className="customCard_caption"
-                        >
-                          {services.subservicetext22}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          className="customCard_caption"
-                        >
-                          {services.subservicetext222}
-                        </Typography>
-                        <Typography variant="h5" className="subservice-head">
-                          {services.subservicehead3}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          className="customCard_caption"
-                        >
-                          {services.subservicetext3}
-                        </Typography>
-                        <Typography variant="h5" className="subservice-head">
-                          {services.subservicehead4}
-                        </Typography>
-                        <Typography
-                          variant="body2"
-                          className="customCard_caption"
-                        >
-                          {services.subservicetext4}
-                        </Typography>
-                      </CardContent>
-                    </CardActionArea>
-                  </Card>
-                </Grid>
-              ) : null}
-            </>
-          ))}
+            {[...new Set(data.services.map((item) => item.tag))].map((tag) => (
+              <Tab
+                label={tag}
+                value={tag}
+                className={
+                  tabValue == tag ? "customTab_item active" : "customTab_item"
+                }
+              />
+            ))}
+          </Tabs>
         </Grid>
-      </Grid>{" "}
-    </Grid>
+        {/**projects */}
+        <Grid Item xs={12}>
+          <Grid container spacing={3}>
+            {data.services.map((services) => (
+              <>
+                {tabValue == services.tag || tabValue == "All" ? (
+                  <Grid Item className="projbox" className="service-wrapper">
+                    <Card className="customCard" xs={12}>
+                      <CardActionArea>
+                        <CardMedia
+                          className="customCard_image"
+                          image={services.image}
+                          title={services.title}
+                        />
+                        <CardContent>
+                          <Typography variant="h5" className="customCard_title">
+                            {services.title}
+                          </Typography>
+
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.caption}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.captionlist}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.captionlist1}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.captionlist2}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.captionlist3}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.captionlist4}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.captionlist5}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.captionlist6}
+                          </Typography>
+                          <Typography variant="h5" className="subservice-head">
+                            {services.subservicehead}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.subservicetext}
+                          </Typography>
+                          <Typography variant="h5" className="subservice-head">
+                            {services.subservicehead2}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.subservicetext2}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.subservicetext22}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.subservicetext222}
+                          </Typography>
+                          <Typography variant="h5" className="subservice-head">
+                            {services.subservicehead3}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.subservicetext3}
+                          </Typography>
+                          <Typography variant="h5" className="subservice-head">
+                            {services.subservicehead4}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.subservicetext4}
+                          </Typography>
+                          <Typography variant="h5" className="subservice-head">
+                            {services.subservicehead5}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.subservicetext5}
+                          </Typography>
+                          <Typography variant="h5" className="subservice-head">
+                            {services.subservicehead6}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            className="customCard_caption"
+                          >
+                            {services.subservicetext6}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  </Grid>
+                ) : null}
+              </>
+            ))}
+          </Grid>
+        </Grid>{" "}
+      </Grid>
+      <Footer />
+    </>
   );
 };
 
